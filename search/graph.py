fac8230f9d36761f0a2a45fa945e9fb2bfd80503
IsiacOrr13 <isiacjorr@gmail.com>
2023-01-24 23:29:58 -0800

    Added bfs functionality

--- search/graph.py
+++ search/graph.py
@@ -1,5 +1,9 @@
+from copy import deepcopy
+
 import networkx as nx
 import pathlib
+from queue import Queue
+
 
 class Graph:
     """
@@ -11,17 +15,80 @@ class Graph:
         """
         Initialization of graph object 
         """
-        self.graph = nx.read_adjlist(filename, create_using=nx.DiGraph, delimiter=";")
+        try:
+            self.graph = nx.read_adjlist(filename, create_using=nx.DiGraph, delimiter=";")
+            self.nodes = self.graph.nodes()
+            self.edges = self.graph.edges()
 
-    def bfs(self, start, end=None):
-        """
-        TODO: write a method that performs a breadth first traversal and pathfinding on graph G
+        except:
+            print('Unreadable file -- Check file name, type, and structure')
 
-        * If there's no end node input, return a list nodes with the order of BFS traversal
-        * If there is an end node input and a path exists, return a list of nodes with the order of the shortest path
-        * If there is an end node input and a path does not exist, return None
+    def bfs_search(self, node):
+        q = Queue()
+        visited = set()
+        q.put(node)
+        while q:
+            curr_node = q.get()
+            if curr_node not in visited:
+                visited.add(curr_node)
+                for edge in self.edges():
+                    if edge[0] == curr_node and edge[1] not in visited:
+                        q.put(edge[1])
+        return visited
 
-        """
-        return
+    def path_bfs(self, node, end):
+        q = Queue()
+        visited = set()
+        q.put(node)
+        path_lens = dict()
+        path_lens[node] = 0
+        prevs = dict()
+        while q:
+            curr_node = q.get()
+            if curr_node not in visited:
+                dist = path_lens[curr_node] + 1
+                visited.add(curr_node)
+                found_end = False
+                for edge in self.edges():
+                    if edge[0] == curr_node and edge[1] not in visited:
+                        q.put(edge[1])
+                        path_lens[edge[1]] = dist
+                        prevs[edge[1]] = curr_node
+                        found_end = edge[1] == end
+                if found_end:
+                    path = [None] * (path_lens[end] + 1)
+                    at_node = end
+                    for idx in range(path_lens[end] + 1):
+                        path[path_lens[end] - idx] = at_node
+                        if at_node != start:
+                            at_node = prevs[at_node]
+                    return path
+        return None
+
+    def bfs(self, node, end=None):
+        if self.nodes() or self.edges():
+            if node in self.nodes():
+                if end:
+                    if end in self.nodes():
+                        return self.path_bfs(node, end)
+                    else:
+                        print('End node not in graph, returning result for end=None')
+                        return self.bfs_search(node)
+                else:
+                    return self.bfs_search(node)
+            else:
+                print('Starting node not in graph')
+                return None
+        else:
+            print('Graph is empty, check file')
+            return None
+
 f = pathlib.Path(__file__).resolve().parent.parent / 'data/tiny_network.adjlist'
+x = Graph(f)
+start = 'Luke Gilbert'
+end = 'Martin Kampmann'
+#end = '31540829'
 
+
+
+print(x.bfs(start, end))
